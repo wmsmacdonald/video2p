96a44931b11a94578c63e954044b146cd289fa4f
William MacDonald <wmsmacdonald@gmail.com>
2015-10-13 16:54:12 -0500

    paren

--- js/peer2.js
+++ js/peer2.js
@@ -51,7 +51,7 @@ function sourceOpen(e) {
                         else {
                             console.log( "Data loss." );
                         }
-                    }
+                    });
 
                 }
 
